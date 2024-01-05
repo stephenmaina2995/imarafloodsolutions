bb51ef33041259610e72c6297cd092f6d32a0dc1
Ngaremaina <ngaremaina4@gmail.com>
2024-01-05 17:07:10 +0300

    Login to server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,18 @@ import Login from './components/login/Login';
 import SignUp from './components/signup/Signup';
 import {Routes, Route} from "react-router-dom"
 function App() {
+  const handleLogin = (user) => {
+    fetch("/login", {
+      method:"POST",
+      headers:{"Content-Type":"application/json"},
+      body:JSON.stringify(user)
+    })
+
+  }
   return (
     <div className="App">
       <Routes>
-        <Route path='/login' element = {<Login />}></Route>
+        <Route path='/login' element = {<Login handleLogin = {handleLogin}/>}></Route>
         <Route path='/signup' element = {<SignUp />}></Route>
       </Routes>
     </div>
